53746f3765d1e83903c8fd4906d1fba427df15d0
Mohammed ISMAILI <mohammedismaili.2004@gmail.com>
2025-05-01 18:05:42 +0100

    Initial commit of VAWG project

---
+++ front/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Map from './components/Map'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Map></Map>
+      </div>
+    </>
+  )
+}
+
+export default App
